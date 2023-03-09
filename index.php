5b54888c2476ad9efab69745de288d9b6906d37c
JbbDHs <119319928+JbbDHs@users.noreply.github.com>
2023-03-09 15:42:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,32 +12,22 @@
 		<?php 
             include_once ("db_funktionen.php");
 			include_once ("menu.php");
-            // -> id des workshops
-            // wenn existent: ausgeben alles dazu
-            // dann: abgleich schüler mit id = id workshop
-            // in tabelle alle mit dieser id (name u vorname)
-            //
-            // workshops müssen von eins aufwärts durchgängig nummeriert werden
-            //
-            // Uafräumen (simone abendroth)
-            // max 10 schüler
-            //
-            // Name     Vorname    Jahrgang
         
-            $idDesWorkshops = 1;
+
+        $residDesworkshops = dbi_query_onlysql("SELECT id from workshops");
             
-        // while(dbi_query_onlysql($idDesWorkshops)=true){
-            //Anpassen der Werte an neuen Workshop    
+        while($datenWorkshop = mysqli_fetch_assoc($residDesworkshops)){    
             
 
-            $resmaxAnzahl = mysqli_fetch_array(dbi_query_onlysql("SELECT `Obergrenze` FROM workshops WHERE id =" . $idDesWorkshops));
+            $resmaxAnzahl = mysqli_fetch_array(dbi_query_onlysql("SELECT `Obergrenze` FROM workshops WHERE id =" . $datenWorkshop['id']));
 
-            $resNameDesWorkshops = mysqli_fetch_array(dbi_query_onlysql("SELECT `Workshop` FROM workshops WHERE id =" . $idDesWorkshops));
+            $resNameDesWorkshops = mysqli_fetch_array(dbi_query_onlysql("SELECT `Workshop` FROM workshops WHERE id =" . $datenWorkshop['id']));
             
-            $resNameDesBetreuers = mysqli_fetch_array(dbi_query_onlysql("SELECT `Betreuer` FROM workshops WHERE id =" . $idDesWorkshops));
+            $resNameDesBetreuers = mysqli_fetch_array(dbi_query_onlysql("SELECT `Betreuer` FROM workshops WHERE id =" . $datenWorkshop['id']));
 
-            $residDesSchülers = dbi_query_onlysql("SELECT id, Name, Vorname, Jahrgang FROM `schüler` WHERE EXISTS (SELECT `id` FROM `schüler` WHERE `Workshop`=".$idDesWorkshops.")");
-            
+            $residDesSchülers = dbi_query_onlysql("SELECT id, Name, Vorname, Jahrgang FROM `schüler` WHERE EXISTS (SELECT `id` FROM `schüler` WHERE `Workshop`=".$datenWorkshop['id'].")");
+
+
             echo "<h2>" . $resNameDesWorkshops[0] . " (" . $resNameDesBetreuers[0] . ")</h2>";
             echo "<h3> Max. " . $resmaxAnzahl[0] . " Schüler </h3>";
 
@@ -50,23 +40,10 @@
             <td>" . mysqli_fetch_array(dbi_query_onlysql("SELECT `Jahr` FROM Jahrgang WHERE id =". $daten['Jahrgang']))[0] . "</td>
             </tr>";
             } echo"</table>";
-            $idDesWorkshops += 1;
-        // }
-        
-        
-        
-
-
-
-
-
-        
-         
-        
-        
-        
+            
+        }
         ?>
-         
+        
  
     </body>
 </html>
